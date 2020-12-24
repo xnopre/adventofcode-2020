40251026528141c1f30870ec10d92fbd9f51d8cf
Xavier NOPRE <xnopre@gmail.com>
2020-12-24 16:30:41 +0100

    Day 24 part 1

--- day24/solve.test.ts
+++ day24/solve.test.ts
@@ -1,13 +1,20 @@
-import { solvePart1, solvePart2 } from "./solve";
+import { getXY, solvePart1, solvePart2 } from "./solve";
 
 describe("My solver", () => {
   it("solves the problem with sample data", () => {
-    expect(solvePart1("day24/data/sample")).toEqual(0);
+    expect(solvePart1("day24/data/sample")).toEqual(10);
     expect(solvePart2("day24/data/sample")).toEqual(0);
   });
 
   it.skip("solves the problem with my sample", () => {
     expect(solvePart1("day24/data/mydata")).toEqual(0);
     expect(solvePart2("day24/data/mydata")).toEqual(0);
+  });
+});
+
+describe("getXY", () => {
+  it("should return X,Y position of tile from directions", () => {
+    expect(getXY("nwwswee")).toEqual({ x: 0, y: 0 });
+    expect(getXY("esew")).toEqual({ x: 1, y: -1 });
   });
 });
--- day24/solve.ts
+++ day24/solve.ts
@@ -1,7 +1,59 @@
 import { readLinesFromInputFile } from "../utils";
 
+export function getXY(directions: string) {
+  let x = 0;
+  let y = 0;
+  let [, direction, rest] = directions.match(/^(ne|se|e|nw|sw|w)(.*)$/);
+  while (true) {
+    switch (direction) {
+      case "e":
+        x += 2;
+        break;
+      case "ne":
+        x += 1;
+        y += 1;
+        break;
+      case "se":
+        x += 1;
+        y -= 1;
+        break;
+      case "w":
+        x -= 2;
+        break;
+      case "nw":
+        x -= 1;
+        y += 1;
+        break;
+      case "sw":
+        x -= 1;
+        y -= 1;
+        break;
+    }
+    // console.log(`${direction} --> ${x},${y}`);
+    if (rest.trim().length === 0) break;
+    [, direction, rest] = rest.match(/^(ne|se|e|nw|sw|w)(.*)$/);
+  }
+  return { x, y };
+}
+
 export function solvePart1(filename: string): number {
-  return 0;
+  const tiles: { [key: string]: boolean } = {};
+  const lines = readLinesFromInputFile(filename);
+  lines.forEach((line) => {
+    const pos = getXY(line);
+    const key = `${pos.x}-${pos.y}`;
+    if (tiles[key] === undefined) {
+      tiles[key] = false;
+    }
+    tiles[key] = !tiles[key];
+  });
+  let count = 0;
+  for (const key in tiles) {
+    if (tiles[key]) {
+      count++;
+    }
+  }
+  return count;
 }
 
 export function solvePart2(filename: string): number {
@@ -9,6 +61,7 @@ export function solvePart2(filename: string): number {
 }
 
 if (require.main === module) {
+  getXY("nwwswee");
   console.log(
     "Day24 - Part 1 : answer is : " + solvePart1("day24/data/mydata")
   );

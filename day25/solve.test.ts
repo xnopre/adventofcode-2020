26e2fd9cb010824e822d9f8f8beadfa9c1e2e861
Xavier NOPRE <xnopre@gmail.com>
2020-12-25 16:29:36 +0100

    Day 25 part 1

--- day25/solve.test.ts
+++ day25/solve.test.ts
@@ -1,13 +1,27 @@
-import { solvePart1, solvePart2 } from "./solve";
+import { encrypt, findLoopSize, solvePart1, solvePart2 } from "./solve";
 
 describe("My solver", () => {
   it("solves the problem with sample data", () => {
-    expect(solvePart1("day25/data/sample")).toEqual(0);
+    expect(solvePart1("day25/data/sample")).toEqual(14897079);
     expect(solvePart2("day25/data/sample")).toEqual(0);
   });
 
-  it.skip("solves the problem with my sample", () => {
-    expect(solvePart1("day25/data/mydata")).toEqual(0);
+  it("solves the problem with my sample", () => {
+    expect(solvePart1("day25/data/mydata")).toEqual(4441893);
     expect(solvePart2("day25/data/mydata")).toEqual(0);
+  });
+});
+
+describe("findLoopSize", () => {
+  it("should find the loop size", () => {
+    expect(findLoopSize(5764801)).toEqual(8);
+    expect(findLoopSize(17807724)).toEqual(11);
+  });
+});
+
+describe("encrypt", () => {
+  it("should encrypt as expected", () => {
+    expect(encrypt(17807724, 8)).toEqual(14897079);
+    expect(encrypt(5764801, 11)).toEqual(14897079);
   });
 });
--- day25/solve.ts
+++ day25/solve.ts
@@ -1,7 +1,34 @@
 import { readLinesFromInputFile } from "../utils";
 
+const SUBJECT_NUMBER = 7;
+
+export function findLoopSize(n: number) {
+  let value = 1;
+  let loopSize = 0;
+  const MAX = 200000000;
+  while (value != n && loopSize < MAX) {
+    value *= SUBJECT_NUMBER;
+    value = value % 20201227;
+    loopSize++;
+  }
+  return loopSize === MAX ? undefined : loopSize;
+}
+
+export function encrypt(subjectNumber: number, loopSize: number) {
+  let value = 1;
+  for (let i = 0; i < loopSize; i++) {
+    value = value * subjectNumber;
+    value = value % 20201227;
+  }
+  return value;
+}
+
 export function solvePart1(filename: string): number {
-  return 0;
+  const lines = readLinesFromInputFile(filename);
+  const cardPublicKey = parseInt(lines[0]);
+  const doorPublicKey = parseInt(lines[1]);
+  const doorLoopSize = findLoopSize(doorPublicKey);
+  return encrypt(cardPublicKey, doorLoopSize);
 }
 
 export function solvePart2(filename: string): number {

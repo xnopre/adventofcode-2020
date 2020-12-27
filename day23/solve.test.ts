a4ba65d4b6e8e8b3b3d752dbe4b241e245866920
Xavier NOPRE <xnopre@gmail.com>
2020-12-27 19:45:09 +0100

    Day 23 : part 1 OK mais part 2 trop longue (faudrait 3,5 jours...)

--- day23/solve.test.ts
+++ day23/solve.test.ts
@@ -1,13 +1,44 @@
-import { solvePart1, solvePart2 } from "./solve";
+import {
+  convertToCupsArray,
+  doMoves,
+  labelsAfter1,
+  move,
+  solvePart1,
+  solvePart2,
+} from "./solve";
 
 describe("My solver", () => {
   it("solves the problem with sample data", () => {
-    expect(solvePart1("day23/data/sample")).toEqual(0);
-    expect(solvePart2("day23/data/sample")).toEqual(0);
+    expect(solvePart1("day23/data/sample")).toEqual("67384529");
+    // expect(solvePart2("day23/data/sample")).toEqual(149245887792);
   });
 
-  it.skip("solves the problem with my sample", () => {
-    expect(solvePart1("day23/data/mydata")).toEqual(0);
-    expect(solvePart2("day23/data/mydata")).toEqual(0);
+  it("solves the problem with my sample", () => {
+    expect(solvePart1("day23/data/mydata")).toEqual("47382659");
+    // expect(solvePart2("day23/data/mydata")).toEqual(0);
+  });
+});
+
+describe("move", () => {
+  it("should do the job", () => {
+    const expected = [2, 8, 9, 1, 5, 4, 6, 7, 3];
+    expect(move(convertToCupsArray("389125467"))).toEqual(expected);
+  });
+  it("should do the job", () => {
+    const expected = [5, 4, 6, 7, 8, 9, 1, 3, 2];
+    expect(move(convertToCupsArray("289154673"))).toEqual(expected);
+  });
+});
+
+describe("doMoves", () => {
+  it("should do the job", () => {
+    const expected = [8, 3, 7, 4, 1, 9, 2, 6, 5];
+    expect(doMoves(convertToCupsArray("389125467"), 10)).toEqual(expected);
+  });
+});
+
+describe("labelsAfter1", () => {
+  it("should do the job", () => {
+    expect(labelsAfter1(convertToCupsArray("837419265"))).toEqual("92658374");
   });
 });
--- day23/solve.ts
+++ day23/solve.ts
@@ -1,11 +1,72 @@
 import { readLinesFromInputFile } from "../utils";
 
-export function solvePart1(filename: string): number {
-  return 0;
+export function convertToCupsArray(cups: string): number[] {
+  return cups.split("").map((s) => parseInt(s));
+}
+
+export function move(cupsArray: number[], min = -1, max = -1): number[] {
+  const currentCupValue = cupsArray.splice(0, 1)[0];
+  const pickedCups = cupsArray.splice(0, 3);
+  if (min === -1) min = Math.min(...cupsArray);
+  if (max === -1) max = Math.max(...cupsArray);
+  let dest = currentCupValue;
+  let destIndex = -1;
+  do {
+    dest--;
+    destIndex = cupsArray.indexOf(dest);
+  } while (destIndex === -1 && dest > min);
+  if (destIndex === -1) {
+    destIndex = cupsArray.indexOf(max);
+  }
+  const newCupsArray = cupsArray
+    .slice(0, destIndex + 1)
+    .concat(pickedCups)
+    .concat(cupsArray.slice(destIndex + 1))
+    .concat([currentCupValue]);
+  return newCupsArray;
+}
+
+export function doMoves(
+  cups: number[],
+  movesCount: number,
+  min = -1,
+  max = -1
+) {
+  for (let i = 0; i < movesCount; i++) {
+    if (i % 100 === 0) {
+      console.log(`move # ${(i * 100) / movesCount} %`);
+    }
+    cups = move(cups, min, max);
+  }
+  return cups;
+}
+
+export function labelsAfter1(cups: number[]) {
+  const cupsStr = cups.map((n) => n.toString()).join("");
+  const indexOf1 = cupsStr.indexOf("1");
+  return cupsStr.substr(indexOf1 + 1) + cupsStr.substr(0, indexOf1);
+}
+
+export function solvePart1(filename: string): string {
+  const cups = convertToCupsArray(readLinesFromInputFile(filename)[0]);
+  const cupsAfter100Moves = doMoves(cups, 100);
+  return labelsAfter1(cupsAfter100Moves);
 }
 
 export function solvePart2(filename: string): number {
-  return 0;
+  const cups = convertToCupsArray(readLinesFromInputFile(filename)[0]);
+  const min = Math.min(...cups);
+  const max = Math.max(...cups);
+  for (let i = max + 1; i <= 1000000; i++) {
+    cups.push(i);
+  }
+  const cupsAfter100Moves = doMoves(cups, 10000000, min, 1000000);
+  const indexOf1 = cupsAfter100Moves.indexOf(1);
+  const v1 = cupsAfter100Moves[indexOf1 + 1];
+  console.log({ v1 });
+  const v2 = cupsAfter100Moves[indexOf1 + 2];
+  console.log({ v2 });
+  return v1 * v2;
 }
 
 if (require.main === module) {
